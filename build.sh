d64233b2490c0f3938b494c9dfcdcf7c915d7bed
carlos.escalona <carlos.escalona@gmail.com>
2022-02-09 12:43:23 -0300

    ajustes iniciales

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 docker build -t techer/asterisk -f Dockerfile.Asterisk .
-docker build -t techer/kamailio -f Dockerfile.Kamailio . 
+# docker build -t techer/kamailio -f Dockerfile.Kamailio . 
 docker build -t techer/ari -f Dockerfile.Ari .
