9e1082f440105daed51670def7b3ffa62d2772c1
carlos.escalona <carlos.escalona@T21>
2022-02-10 10:18:20 -0300

    coisas

--- ari-app/app.js
+++ ari-app/app.js
@@ -1,18 +1,36 @@
-var client = require('ari-client')
-client.connect('http://asterisk:8088', 'asterisk', 'asterisk', clientLoaded);
+/*jshint node:true*/
+var ari = require('ari-client')
 
-function clientLoaded (err, client) {
-    if (err) {
-      throw err;
-    }
-    // handler for StasisStart event
-    function stasisStart(event, channel) {
-      // ensure the channel is not a dialed channel
-      console.log("Statis Start!!!!", event);
+
+async function main(){
+  
+  const client = await ari.connect('http://asterisk:8088', 'asterisk', 'asterisk');
+
+  // handler for StasisStart event
+  function stasisStart(event, channel) {
+    // ensure the channel is not a dialed channel
+    console.log("Statis Start!!!!", event);
+    
+
+    var dialed = client.Channel();
+
+    client.channels.create({
       
-    }
-  
-    client.on('StasisStart', stasisStart);
-  
-    client.start('ari-app');
+    })
+
   }
+
+  client.on('StasisStart', stasisStart);
+
+  client.start('ari-app');
+}
+
+
+(async () => {
+  try {
+      await main();
+  } catch (e) {
+      console.error(e);
+  }
+})();
+
