90d5bdf245660a87cc923394ac6d9b3cbd2dd48f
carlos.escalona <carlos.escalona@T21>
2022-02-10 15:54:09 -0300

    ajustes

--- ari-app/app.js
+++ ari-app/app.js
@@ -1,8 +1,6 @@
 /*jshint node:true*/
 var ari = require('ari-client')
 
-var dialogs = [];
-
 async function main(){
   
   const client = await ari.connect('http://asterisk:8088', 'asterisk', 'asterisk');
@@ -10,27 +8,28 @@ async function main(){
   // handler for StasisStart event
   async function stasisStart(event, channel) {
     
-    console.log(event);
-    if(event.channel.state != 'Down'){      
-      const dialed = await client.channels.create({
-        endpoint: `PJSIP/${event.channel.dialplan.exten}`,
-        app: 'ari-app',
-        appArgs: 'dialed'
-      });
-
-      let bridge = client.Bridge();
-
-      bridge = await bridge.create();
-      console.log(dialed.id);
-      await bridge.addChannel({channel: [channel.id, dialed.id]});
-
-      await client.channels.dial({ channelId: dialed.id});
-
+    if(event.appArgs[0] != 'dialed'){      
+      await call(channel, event.channel.dialplan.exten)
     }
   }
   
   client.on('StasisStart', stasisStart);
   client.start('ari-app');
+}
+
+async function call(origem, extension){
+  const dialed = await client.channels.create({
+    endpoint: `PJSIP/${extension}`,
+    app: 'ari-app',
+    appArgs: 'dialed'
+  });
+
+  let bridge = client.Bridge();
+
+  bridge = await bridge.create();
+  await bridge.addChannel({channel: [origem.id, dialed.id]});
+
+  await client.channels.dial({ channelId: dialed.id});
 }
 
 
