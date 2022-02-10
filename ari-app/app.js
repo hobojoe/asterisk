e40903d7f3d912fbccbaa2e042000245330fe314
carlos.escalona <carlos.escalona@T21>
2022-02-10 14:23:32 -0300

    cosas

--- ari-app/app.js
+++ ari-app/app.js
@@ -1,27 +1,44 @@
 /*jshint node:true*/
 var ari = require('ari-client')
 
+var dialogs = [];
 
 async function main(){
   
   const client = await ari.connect('http://asterisk:8088', 'asterisk', 'asterisk');
 
   // handler for StasisStart event
-  function stasisStart(event, channel) {
-    // ensure the channel is not a dialed channel
-    console.log("Statis Start!!!!", event);
+  async function stasisStart(event, channel) {
     
+    // ensure the channel is not a dialed channel
+    // console.log("Statis Start!!!!", event);
+    console.log(event);
+    if(event.channel.state != 'Down'){
+      channel.anwers();
+      const dialed = await client.channels.create({
+        endpoint: `PJSIP/${event.channel.dialplan.exten}`,
+        app: 'ari-app',
+        appArgs: 'dialed'
+      });
 
-    var dialed = client.Channel();
+      // for (const key in dialed) {
+      //   if(dialed.hasOwnProperty(key))
+      //     console.log(dialed[key]);
+      // }
 
-    client.channels.create({
-      
-    })
+      //colocar en un bridge
 
+      let bridge = client.Bridge();
+
+      bridge = await bridge.create();
+
+      await bridge.addChannel({channel: [channel.id, dialed.id]});
+
+      await client.channels.dial({ channelId: dialed.id});
+    }
   }
-
+  
   client.on('StasisStart', stasisStart);
-
   client.start('ari-app');
 }
 
