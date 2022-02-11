7599da2dbeff747e71a6c7f86579dfc361d16c26
Luciano Luciano <luciano@techer.com.br>
2022-02-11 15:25:05 -0300

    v3

--- ari-app/app.js
+++ ari-app/app.js
@@ -2,54 +2,68 @@
 var ari = require('ari-client');
 
 
-async function main(){
+
+async function main() {
   const client = await ari.connect('http://asterisk:8088', 'asterisk', 'asterisk');
 
   // handler for StasisStart event
   async function stasisStart(event, channel) {
     // console.log('ligou', event);
-    if(event.args[0] != 'dialed'){      
+    if (event.args[0] != 'dialed') {
       await makeCall(channel, event.channel.dialplan.exten, client)
     }
   }
-  
+
   client.on('StasisStart', stasisStart);
   client.start('ari-app');
 }
 
-async function makeCall(origem, extension, client){
+async function makeCall(origem, extension, client) {
   const dialed = await client.channels.create({
     endpoint: `PJSIP/${extension}`,
     app: 'ari-app',
     appArgs: 'dialed'
   });
 
-  origem.on('StasisEnd', async function(event, channel) {
+  origem.on('StasisEnd', async function (event, channel) {
     await hangup(dialed);
   });
 
-  dialed.on('ChannelDestroyed', async function(event, channel) {
+  dialed.on('ChannelDestroyed', async function (event, channel) {
     await hangup(origem);
   });
 
-  let bridge = client.Bridge();
-  bridge = await bridge.create();
+
   // console.log('bridge', bridge);
   // client.bridges.record({
   //   bridgeId: bridge.id, 
   //   format: "wav", 
   //   name: "testeluciano"
   // })
-  await bridge.addChannel({channel: [origem.id, dialed.id]});
+  let bridge = client.Bridge();
+  bridge = await bridge.create({ type: 'mixing' });
 
-  await client.channels.dial({ channelId: dialed.id});
+  dialed.on('ChannelStateChange', async function (evento, canal) {
+    console.log('canal', [origem.id, canal.id]);
+    console.log('canal estado', canal.state)
+
+    if (canal.state == "Up") {
+      console.log('up', canal.state)
+      await bridge.addChannel({ channel: canal.id });
+    }
+  });
+
+  await bridge.addChannel({ channel: origem.id });
+
+  await bridge.startMoh();
+  // await client.channels.dial({ channelId: dialed.id });
 }
 
 
 async function hangup(channel) {
-  try{
+  try {
     await channel.hangup();
-  }catch(e){
+  } catch (e) {
 
   }
 }
@@ -57,9 +71,9 @@ async function hangup(channel) {
 
 (async () => {
   try {
-      await main();
+    await main();
   } catch (e) {
-      console.error(e);
+    console.error(e);
   }
 })();
 
