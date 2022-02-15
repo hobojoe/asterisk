a8fb305c5609591f66e2c6eb09cde8bf5a503200
Luciano Luciano <luciano@techer.com.br>
2022-02-15 15:32:05 -0300

    v2

--- ari-app/app.js
+++ ari-app/app.js
@@ -1,79 +1,178 @@
 /*jshint node:true*/
+'use strict';
+
 var ari = require('ari-client');
+var util = require('util');
 
+// ensure endpoint was passed in to script
+// if (!process.argv[2]) {
+//   console.error('usage: node ari-app.js endpoint');
+//   process.exit(1);
+// }
 
+ari.connect('http://asterisk:8088', 'asterisk', 'asterisk', clientLoaded);
 
-async function main() {
-  const client = await ari.connect('http://asterisk:8088', 'asterisk', 'asterisk');
+// handler for client being loaded
+function clientLoaded(err, client) {
+  if (err) {
+    throw err;
+  }
 
   // handler for StasisStart event
-  async function stasisStart(event, channel) {
-    // console.log('ligou', event);
-    if (event.args[0] != 'dialed') {
-      await makeCall(channel, event.channel.dialplan.exten, client)
+  function stasisStart(event, channel) {
+    // ensure the channel is not a dialed channel
+    var dialed = event.args[0] === 'dialed';
+
+    if (!dialed) {
+      channel.answer(function (err) {
+        if (err) {
+          throw err;
+        }
+
+        console.log('Channel %s has entered our application', channel.name);
+
+        findOrCreateHoldingBridge(channel, event.channel.dialplan.exten);
+      });
     }
+  }
+
+  function findOrCreateHoldingBridge(channel, extension) {
+    client.bridges.list(function (err, bridges) {
+      var holdingBridge;
+      if (bridges) {
+        holdingBridge = bridges.filter(function (candidate) {
+          return candidate.bridge_type === 'holding';
+        })[0];
+      }
+
+
+      if (holdingBridge) {
+        console.log('Using existing holding bridge %s', holdingBridge.id);
+
+        originate(channel, holdingBridge, extension);
+      } else {
+        client.bridges.create({ type: 'holding' }, function (err, holdingBridge) {
+          if (err) {
+            throw err;
+          }
+
+          console.log('Created new holding bridge %s', holdingBridge.id);
+
+          originate(channel, holdingBridge, extension);
+        });
+      }
+    });
+  }
+
+  function originate(channel, holdingBridge, extension) {
+    holdingBridge.addChannel({ channel: channel.id }, function (err) {
+      if (err) {
+        throw err;
+      }
+
+      //If disable Moh everything works.
+      holdingBridge.startMoh(function(err) {
+        // ignore error
+      });
+    });
+
+    var dialed = client.Channel();
+
+    channel.on('StasisEnd', function (event, channel) {
+      safeHangup(dialed);
+    });
+
+    dialed.on('ChannelDestroyed', function (event, dialed) {
+      safeHangup(channel);
+    });
+
+    dialed.on('StasisStart', function (event, dialed) {
+      joinMixingBridge(channel, dialed, holdingBridge);
+    });
+
+    dialed.originate(
+      { endpoint: `PJSIP/${extension}`, app: 'ari-app', appArgs: 'dialed' },
+      function (err, dialed) {
+        if (err) {
+          throw err;
+        }
+      });
+  }
+
+  // safely hangs the given channel
+  function safeHangup(channel) {
+    console.log('Hanging up channel %s', channel.name);
+
+    channel.hangup(function (err) {
+      // ignore error
+    });
+  }
+
+  // handler for dialed channel entering Stasis
+  function joinMixingBridge(channel, dialed, holdingBridge) {
+    var mixingBridge = client.Bridge();
+
+    dialed.on('StasisEnd', function (event, dialed) {
+      dialedExit(dialed, mixingBridge);
+    });
+
+    dialed.answer(function (err) {
+      if (err) {
+        throw err;
+      }
+    });
+
+    mixingBridge.create({ type: 'mixing' }, function (err, mixingBridge) {
+      if (err) {
+        throw err;
+      }
+
+      console.log('Created mixing bridge %s', mixingBridge.id);
+
+      // If Enable Recording, everything works
+      // client.bridges.record({
+      //   bridgeId: mixingBridge.id,
+      //   format: "wav",
+      //   name: "test_bridge_moh" + Math.floor(Math.random() * (1000 - 1 + 1)) + 1
+      // })
+
+      moveToMixingBridge(channel, dialed, mixingBridge, holdingBridge);
+    });
+  }
+
+  // handler for the dialed channel leaving Stasis
+  function dialedExit(dialed, mixingBridge) {
+    console.log(
+      'Dialed channel %s has left our application, destroying mixing bridge %s',
+      dialed.name, mixingBridge.id);
+
+    mixingBridge.destroy(function (err) {
+      if (err) {
+        throw err;
+      }
+    });
+  }
+
+  // handler for new mixing bridge ready for channels to be added to it
+  function moveToMixingBridge(channel, dialed, mixingBridge, holdingBridge) {
+    console.log('Adding channel %s and dialed channel %s to bridge %s',
+      channel.name, dialed.name, mixingBridge.id);
+
+    holdingBridge.removeChannel({ channel: channel.id }, function (err) {
+      if (err) {
+        throw err;
+      }
+
+      mixingBridge.addChannel(
+        { channel: [channel.id, dialed.id] }, function (err) {
+          if (err) {
+            throw err;
+          }
+        });
+    });
   }
 
   client.on('StasisStart', stasisStart);
+
   client.start('ari-app');
 }
-
-async function makeCall(origem, extension, client) {
-  const dialed = await client.channels.create({
-    endpoint: `PJSIP/${extension}`,
-    app: 'ari-app',
-    appArgs: 'dialed'
-  });
-
-  origem.on('StasisEnd', async function (event, channel) {
-    await hangup(dialed);
-  });
-
-  dialed.on('ChannelDestroyed', async function (event, channel) {
-    await hangup(origem);
-  });
-
-
-  // console.log('bridge', bridge);
-  // client.bridges.record({
-  //   bridgeId: bridge.id, 
-  //   format: "wav", 
-  //   name: "testeluciano"
-  // })
-  let bridge = client.Bridge();
-  bridge = await bridge.create({ type: 'mixing' });
-
-  dialed.on('ChannelStateChange', async function (evento, canal) {
-    console.log('canal', [origem.id, canal.id]);
-    console.log('canal estado', canal.state)
-
-    if (canal.state == "Up") {
-      console.log('up', canal.state)
-      await bridge.addChannel({ channel: canal.id });
-    }
-  });
-
-  await bridge.addChannel({ channel: origem.id });
-
-  await bridge.startMoh();
-  // await client.channels.dial({ channelId: dialed.id });
-}
-
-
-async function hangup(channel) {
-  try {
-    await channel.hangup();
-  } catch (e) {
-
-  }
-}
-
-
-(async () => {
-  try {
-    await main();
-  } catch (e) {
-    console.error(e);
-  }
-})();
-
